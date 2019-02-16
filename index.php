bbc62e44efba11097b1b81208ffb42629c1c56bd
Warreronavtki <47694162+Warreronavtki@users.noreply.github.com>
2019-02-16 18:01:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+<title>GAME</title>
+</head>
+<body>
+	<object>
+<embed width="550px" height="400px" align="" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="крестики нолики" quality="high" src="http://allnewsru.tk/game/game.swf">
+</object>
+</body>
+</html>
